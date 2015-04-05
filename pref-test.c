721184209f224e97f71b9c20ec811b74aff45bb1
Jan Vesely <jan.vesely@rutgers.edu>
2015-04-05 18:38:11 -0400

    Add memory allocation, irq disabling, X86 check
    Signed-off-by: Jan Vesely <jan.vesely@rutgers.edu>

--- pref-test.c
+++ pref-test.c
@@ -1,14 +1,53 @@
 #include <linux/init.h>
 #include <linux/module.h>
+#include <linux/vmalloc.h>
+
+#ifndef CONFIG_X86
+# error "This module only works on X86"
+#endif
 
 MODULE_LICENSE("GPL");
 
-static uint iterations;
+static uint iterations = 1000;
 module_param(iterations, uint, S_IRUGO);
 
 static int __init fake_init(void)
 {
-	printk(KERN_ALERT "Hello, world\n");
+	char * data;
+	uint i;
+	char res = 0;
+	ulong irqs;
+
+	printk(KERN_INFO "Allocating memory\n");
+	data = vmalloc(PAGE_SIZE);
+
+	/* Setup TLB miss, and cache miss counters */
+
+	/* Disable interrupts */
+	local_irq_save(irqs);
+
+	/* Read TLB miss and Cache miss counters */
+
+	for (i = 0; i < iterations; ++i) {
+		/* Flush TLB entry */
+
+		/* Flush both PTE cachelines */
+
+		/* Access data to trigger page walk */
+		res ^= data[0];
+
+		/* Access PTE in second cacheline */
+	}
+
+	/* Read the counters again */
+
+	/* Enable interrupts */
+	local_irq_restore(irqs);
+
+	/* Clean up counters */
+
+	printk(KERN_INFO "Freeing allocated memory\n");
+	vfree(data);
 	return 0;
 }
 
